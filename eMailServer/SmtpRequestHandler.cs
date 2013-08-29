8eedb9f4e3839a7dcbbb3c6356ed2575233105a6
Ringo Leese <r.leese@locr.com>
2013-08-29 14:47:50 +0200

    mehr logging

--- eMailServer/SmtpRequestHandler.cs
+++ eMailServer/SmtpRequestHandler.cs
@@ -13,12 +13,18 @@ namespace eMailServer {
 		private static Logger logger = LogManager.GetCurrentClassLogger();
 
 		private NetworkStream _stream = null;
+		private IPEndPoint _remoteEndPoint = null;
 
 		public SmtpRequestHandler() {
 
 		}
 
 		public SmtpRequestHandler(TcpClient client) {
+			this._remoteEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+			if (this._remoteEndPoint != null) {
+				logger.Debug("connected to {0}:{1}", this._remoteEndPoint.Address.ToString(), this._remoteEndPoint.Port);
+			}
+
 			this._stream = client.GetStream();
 
 			this.SendMessage("service ready", 220);
@@ -37,7 +43,7 @@ namespace eMailServer {
 
 					if (!dataStarted) {
 						incomingMessage = incomingMessage.Trim();
-						logger.Info(String.Format("Received: \"{0}\"", incomingMessage));
+						logger.Info(String.Format("[{0}:{1}] Received: \"{2}\"", this._remoteEndPoint.Address.ToString(), this._remoteEndPoint.Port, incomingMessage));
 
 						if (incomingMessage.StartsWith("HELO ")) {
 							mail.SetClientName(incomingMessage.Substring(5));
@@ -53,18 +59,18 @@ namespace eMailServer {
 							dataStarted = true;
 						} else if (incomingMessage == "QUIT") {
 							if (eMailServer.Options.Verbose) {
-								logger.Debug("quit connection");
+								logger.Debug("[{0}:{1}] quit connection", this._remoteEndPoint.Address.ToString(), this._remoteEndPoint.Port);
 							}
 							return;
 						} else {
 							if (eMailServer.Options.Verbose) {
-								logger.Debug("unknown command: {0}", incomingMessage);
+								logger.Debug("[{0}:{1}] unknown command: {2}", this._remoteEndPoint.Address.ToString(), this._remoteEndPoint.Port, incomingMessage);
 							}
 						}
 					} else {
 						if (incomingMessage.Trim() == ".") {
 							mailMessage = mailMessage.Trim();
-							logger.Info("eMail data received: " + mailMessage);
+							logger.Info("[{0}:{1}] eMail data received: {2}", this._remoteEndPoint.Address.ToString(), this._remoteEndPoint.Port, mailMessage);
 							dataStarted = false;
 
 							mail.ParseData(mailMessage);
@@ -88,7 +94,7 @@ namespace eMailServer {
 		private void SendMessage(string message, int status) {
 			byte[] msg = Encoding.UTF8.GetBytes(String.Format("{0} {1}\n", status, message));
 			this._stream.Write(msg, 0, msg.Length);
-			logger.Info(String.Format("Sent: {0}", message));
+			logger.Info(String.Format("[{0}:{1}] message sent: {2}", this._remoteEndPoint.Address.ToString(), this._remoteEndPoint.Port, message));
 		}
 
 		public void ProcessRequest() {
